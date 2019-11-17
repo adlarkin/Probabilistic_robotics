2851f280dd5d0ba352d2f7c9b5e7d48656941271
Ashton Larkin <ashton@larkinfam.com>
2019-11-17 12:20:02 -0700

    corrected motion equations

--- ParticleFilter/mcl.py
+++ ParticleFilter/mcl.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     num_landmarks = len(lm_x)
 
     # control inputs - noise free (NOT ground truth)
-    v_c = 1 + (.5*cos(2*np.pi*.2*t))
+    v_c = 1 + (.5*sin(2*np.pi*.2*t))
     om_c = -.2 + (2*cos(2*np.pi*.6*t))
 
     # ground truth inputs (with noise)

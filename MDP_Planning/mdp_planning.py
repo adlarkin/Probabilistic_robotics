68fc1106807c2f8a92052b819e4dce815892b0ba
Ashton Larkin <ashton@larkinfam.com>
2019-11-21 15:10:58 -0700

    got basic world working

--- MDP_Planning/mdp_planning.py
+++ MDP_Planning/mdp_planning.py
@@ -40,13 +40,6 @@ if __name__ == "__main__":
     discount_factor = 1
     m_cost = -3  # cost associated with moving
 
-    # # probabilities associated with moving as commanded
-    # # (straight = commanded, non_straight = left/right of commanded)
-    # p_straight = .8
-    # p_non_straight = .1
-
-    my_dtype = np.int32
-
     '''
     file_data = loadmat("mdp_data.mat")
     goal = np.rot90(file_data['goal'])[1:-1 , 1:-1]
@@ -64,67 +57,107 @@ if __name__ == "__main__":
     '''
 
 
-    non_free_states = np.zeros((5,6), dtype=my_dtype)
+    non_free_states = np.zeros((5,6), dtype=np.int8)
     non_free_states[:,0] = 1
     non_free_states[0,:] = 1
     non_free_states[-1,:] = 1
     non_free_states[:,-1] = 1
     non_free_states[2,2] = 1
     non_free_states[1:1+2,-2] = 1
-    goal = np.zeros(non_free_states.shape, dtype=non_free_states.dtype)
+    goal = np.zeros(non_free_states.shape, dtype=np.int8)
     goal[1:1+2,-2] = 1
     obstacles = non_free_states - goal
     # assign values to the goal cells
-    goal_vals = np.zeros(non_free_states.shape, dtype=non_free_states.dtype)
+    goal_vals = np.zeros(non_free_states.shape, dtype=np.float)
     goal_vals[1,-2] = 100
     goal_vals[2,-2] = -100
 
     # values in each cell from value iteration (initially 0 in free cells)
-    state_vals = np.zeros(non_free_states.shape, dtype=non_free_states.dtype)
+    state_vals = np.zeros(non_free_states.shape, dtype=np.float)
     # set the obstacles in the world to a large negative value
     state_vals[np.where(obstacles == 1)] = -5000
     # set the goal cells to their respective values
     goal_idxs = np.where(goal == 1)
     state_vals[goal_idxs] = goal_vals[goal_idxs]
+    print(state_vals,"\n")
+
+    policy = np.zeros(state_vals.shape, dtype=np.int8)
+    policy[:,:] = -1
+
+    converged = False
+    while (not converged):
+        original_vals = np.array(state_vals)
+
+        # ignore edges since they are walls
+        for r in range(1,state_vals.shape[0]-1):
+            for c in range(1,state_vals.shape[1]-1):
+                if non_free_states[r,c]:
+                    continue
+                
+                start = (r,c)
+
+                # north
+                pos_list = [(r-1,c) , (r,c-1) , (r,c+1)]
+                north = try_action(start, pos_list, state_vals, obstacles, m_cost)
+                
+                # east
+                pos_list = [(r,c+1) , (r+1,c) , (r-1,c)]
+                east = try_action(start, pos_list, state_vals, obstacles, m_cost)
+
+                # south
+                pos_list = [(r+1,c) , (r,c-1) , (r,c+1)]
+                south = try_action(start, pos_list, state_vals, obstacles, m_cost)
+
+                # west
+                pos_list = [(r,c-1) , (r+1,c) , (r-1,c)]
+                west = try_action(start, pos_list, state_vals, obstacles, m_cost)
+
+                # find the best action and update the values and policy
+                all_actions = np.array([north, east, south, west])
+                best_action = np.argmax(all_actions)
+                max_val = all_actions[best_action]
+                state_vals[start] = discount_factor * max_val
+                policy[start] = best_action # 0=n, 1=e, 2=s, 3=w
+
+        if np.sum(original_vals - state_vals) == 0:
+            converged = True
+
+    # ignore the wall border
+    non_free_states = non_free_states[1:-1 , 1:-1]
+    state_vals = state_vals[1:-1 , 1:-1]
+    policy = policy[1:-1 , 1:-1]
     print(state_vals)
+    print(policy)  
 
-    for r in range(0,state_vals.shape[0]):
-        for c in range(0,state_vals.shape[1]):
-            if non_free_states[r,c]:
-                continue
-
-            if not ((r == 1) and (c == 3)):
-                continue
-            
-            start = (r,c)
-
-            # north
-            pos_list = [(r-1,c) , (r,c-1) , (r,c+1)]
-            north = try_action(start, pos_list, state_vals, obstacles, m_cost)
-            
-            # south
-            pos_list = [(r,c+1) , (r+1,c) , (r-1,c)]
-            south = try_action(start, pos_list, state_vals, obstacles, m_cost)
-
-            # east
-            pos_list = [(r+1,c) , (r,c-1) , (r,c+1)]
-            east = try_action(start, pos_list, state_vals, obstacles, m_cost)
-
-            # west
-            pos_list = [(r,c-1) , (r+1,c) , (r-1,c)]
-            west = try_action(start, pos_list, state_vals, obstacles, m_cost)
-
-            all_actions = np.array([north, south, east, west])
-            best_action = np.argmax(all_actions)
-            max_val = all_actions[best_action]
-            state_vals[start] = max_val
-            print(max_val, best_action)
-
-
+    '''
     plt.subplot(121)
     plt.imshow(obstacles)
     plt.title("obstacles")
     plt.subplot(122)
     plt.imshow(goal)
     plt.title('goal')
+    plt.show()
+    '''
+
+    arrow_map_dx = {}
+    arrow_map_dx[0] = 0
+    arrow_map_dx[1] = .4
+    arrow_map_dx[2] = 0
+    arrow_map_dx[3] = -.4
+
+    arrow_map_dy = {}
+    arrow_map_dy[0] = -.4
+    arrow_map_dy[1] = 0
+    arrow_map_dy[2] = .4
+    arrow_map_dy[3] = 0
+    
+    plt.imshow(state_vals)
+    for r in range(policy.shape[0]):
+        for c in range(policy.shape[1]):
+            p = policy[r,c]
+            if p < 0:
+                continue
+            plt.arrow(c, r, arrow_map_dx[p], arrow_map_dy[p], 
+                head_width=0.1, head_length=0.1, fc='k', ec='k')
+    plt.title("result")
     plt.show()
